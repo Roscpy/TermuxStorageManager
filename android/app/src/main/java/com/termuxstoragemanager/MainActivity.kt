56931fc949d06663dea620474567fc7467ea3d28
Roscpy <livensonmilien44@gmail.com>
2026-05-25 19:34:53 -0400

    fix: remove onCreate, fix Intent type

--- android/app/src/main/java/com/termuxstoragemanager/MainActivity.kt
+++ android/app/src/main/java/com/termuxstoragemanager/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.termuxstoragemanager
 
 import android.content.Intent
-import android.os.Bundle
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled

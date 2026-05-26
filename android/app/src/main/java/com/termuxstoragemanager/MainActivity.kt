b68de57546f5aced9ae7b0b0d9ff1881ec974c88
Roscpy <livensonmilien44@gmail.com>
2026-05-25 20:02:27 -0400

    fix: remove redundant import

--- android/app/src/main/java/com/termuxstoragemanager/MainActivity.kt
+++ android/app/src/main/java/com/termuxstoragemanager/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.termuxstoragemanager
 
 import android.content.Intent
-import com.termuxstoragemanager.StorageManagerModule
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled

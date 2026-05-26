15114110accdf01087dcc62732683ebe16350d29
Roscpy <livensonmilien44@gmail.com>
2026-05-26 15:54:07 -0400

    fix: correct StorageManagerPackage inheritance and syntax

--- android/app/src/main/java/com/termuxstoragemanager/StorageManagerPackage.kt
+++ android/app/src/main/java/com/termuxstoragemanager/StorageManagerPackage.kt
@@ -1,13 +1,20 @@
 package com.termuxstoragemanager
 
+import android.view.View
 import com.facebook.react.ReactPackage
 import com.facebook.react.bridge.NativeModule
 import com.facebook.react.bridge.ReactApplicationContext
+import com.facebook.react.uimanager.ReactShadowNode
 import com.facebook.react.uimanager.ViewManager
+import java.util.ArrayList
 
 class StorageManagerPackage : ReactPackage {
+
     override fun createNativeModules(reactContext: ReactApplicationContext): List<NativeModule> {
-        return listOf(StorageManagerModule(reactContext))
+        val modules = ArrayList<NativeModule>()
+        // On ajoute ton module de gestion de stockage ici
+        modules.add(StorageManagerModule(reactContext))
+        return modules
     }
 
     override fun createViewManagers(reactContext: ReactApplicationContext): List<ViewManager<*, *>> {
